2a6386cffa425c27e0fb59377a9f087d20f84665
margareth91 <margerytka@wp.eu>
2024-03-20 14:41:19 +0100

    added paging for post_list view

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, "blog_app/post/list.html", {"posts": posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # Trzy posty na każdej stronie.
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Jeżeli zmienna page nie jest liczbą całkowitą, wówczas pobierana jest pierwsza strona wyników.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Jeżeli zmienna page ma wartość większą niż numer ostatniej strony wyników, wtedy pobierana jest ostatnia
+        # strona wyników.
+        posts = paginator.page(paginator.num_pages)
+    return render(request, "blog_app/post/list.html", {"page": page, "posts": posts})
 
 
 def post_detail(request, year, month, day, post):
