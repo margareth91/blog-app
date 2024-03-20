1b9d1c874b54192c98d5ec9331adcece5201097c
margareth91 <margerytka@wp.eu>
2024-03-20 15:10:00 +0100

    changed post_list function to class PostListView

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render, get_object_or_404
+
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # Trzy posty na każdej stronie.
-    page = request.GET.get("page")
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # Jeżeli zmienna page nie jest liczbą całkowitą, wówczas pobierana jest pierwsza strona wyników.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Jeżeli zmienna page ma wartość większą niż numer ostatniej strony wyników, wtedy pobierana jest ostatnia
-        # strona wyników.
-        posts = paginator.page(paginator.num_pages)
-    return render(request, "blog_app/post/list.html", {"page": page, "posts": posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 5
+    template_name = "blog_app/post/list.html"
+
+
+# Poprzednia wersja widoku jako funkcji.
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # Trzy posty na każdej stronie.
+#     page = request.GET.get("page")
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # Jeżeli zmienna page nie jest liczbą całkowitą, wówczas pobierana jest pierwsza strona wyników.
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # Jeżeli zmienna page ma wartość większą niż numer ostatniej strony wyników, wtedy pobierana jest ostatnia
+#         # strona wyników.
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, "blog_app/post/list.html", {"page": page, "posts": posts})
 
 
 def post_detail(request, year, month, day, post):
