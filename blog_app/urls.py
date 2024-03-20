1a275eca35ead23daef84a167e8a28f80381f8bb
margareth91 <margerytka@wp.eu>
2024-03-20 15:13:30 +0100

    changed blog_app url from post_list to PostListView

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+
 from . import views
 
 app_name = "blog_app"
 
 urlpatterns = [
     # Widoki posta.
-    path("", views.post_list, name="post_list"),
+    # Poprzednio używany widok jako funkcja.
+    # path("", views.post_list, name="post_list"),
+    path("", views.PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
         views.post_detail,
